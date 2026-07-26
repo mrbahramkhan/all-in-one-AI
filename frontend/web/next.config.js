61fbcfbb9bfab4472a6e4b1130e6b221ab1f3948
mrbahramkhan <mrbkglt@gmail.com>
2026-07-26 18:17:43 +0500

    fix: next.config.js

--- frontend/web/next.config.js
+++ frontend/web/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
-  },
+  env: { NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001' },
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
 };
